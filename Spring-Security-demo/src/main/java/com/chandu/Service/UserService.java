c5054de0a5d05db13dd9e2e9de58511c08f4fb45
chandu <chanduraaparthi@gmail.com>
2024-08-27 20:38:56 +0530

    added BCrypt password encoder

--- Spring-Security-demo/src/main/java/com/chandu/Service/UserService.java
+++ Spring-Security-demo/src/main/java/com/chandu/Service/UserService.java
@@ -1,6 +1,7 @@
 package com.chandu.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.chandu.Repo_OR_Dao.UserRepo;
@@ -13,7 +14,11 @@ public class UserService {
 	@Autowired
 	private UserRepo userRepo;
 	
+	private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+	
 	public User saveUSer(User user) {
+		user.setPassword(encoder.encode(user.getPassword()));
+		System.out.println(user.getPassword());
 		return userRepo.save(user);
 	}
 	
